29c77bd77a08d0ab621c0909a16d26075eed672c
Vadim <vadimkaz.alx@gmail.com>
2022-11-22 10:23:37 +0300

    Update new_form.php

--- new_form.php
+++ new_form.php
@@ -1,5 +1,11 @@
 <?php
 
+function get_row($data) 
+{
+  $data->execute();
+  return $data->fetch(PDO::FETCH_ASSOC);
+}
+
 function valid($fieldName)
 {
    if (!isset($_POST[$fieldName])
@@ -18,31 +24,36 @@ function blocked($username)
    $attemptsBeforeBlocking = 3;
    $sTimeOut = 60;
 
-   $sample   = $mysqli->query('SELECT loginTime, failsCount FROM users WHERE user =' . $username);  
-   $userData = $sample->fetch_assoc();  
- 
+   $sample = prepare_data('SELECT loginTime, failsCount FROM users WHERE user = ?');
+   $sample->bindParam(1, $username, PDO::PARAM_STR);
+   $userData = get_row($sample);
    if ($userData['failsCount'] >= $attemptsBeforeBlocking) 
       if (time() - strtotime($userData['loginTime']) < $sTimeOut)
-          return true
-
+          return true;
+	      
    return false;
 }
 
-function succeed()
+function succeed($username)
 {
     $avatar = $row['avatar'];  
 
     $html .= "<p>Welcome to the password protected area {$user}</p>";  
-    $html .= "<img src=\"{$avatar}\" />";  
-
-    $mysqli->query('UPDATE users SET failsCount = 0, loginTime = ' . time() . ' WHERE user = ' . $username);  
+    $html .= "<img src=\"{$avatar}\" />";
+    $time = time()
+    $sample = prepare_data('UPDATE users SET failsCount = 0, loginTime = ? WHERE user = ?')
+    $sample->bindParam(1, $time, PDO::PARAM_INT);
+    $sample->bindParam(2, $username, PDO::PARAM_STR);
+    $sample->execute();
 }
 
-function failed()
+function failed($username)
 {
    $html .= "<pre><br />Username and/or password incorrect.</pre>";
-
-   $mysqli->query('UPDATE users SET failsCount = failsCount + 1 WHERE user = ' . $username);  
+	
+   $sample = prepare_data('UPDATE users SET failsCount = failsCount + 1 WHERE user = ?');
+   $sample->bindParam(1, $username, PDO::PARAM_STR);
+   $sample->execute();
 }
 
 //------------------------main---------------------------
@@ -55,13 +66,17 @@ if((mb_strlen($_POST['user']) > 50) or (mb_strlen($_POST['pass']) > 25){
 $user = htmlspecialchars ($_POST[ 'username' ]);          //CWE-598 (Get with confidential data (change to POST)) //CWE-79 (Improper Neutralization of Input During Web Page Generation)
 $password = crypt(htmlspecialchars ($_POST['password']), $someSalt)); //CWE-327 (Weak crypto alg (changed to sha256)), CWE-759 (One-way hash without salt (hash with salt))
 
-$sample  = $mysqli->query('SELECT * FROM users WHERE user = ' . $user . ' AND password = ' . $password;
+$sample  = prepare_data('SELECT * FROM users WHERE user = ? AND password = ?);
+$sample->bindParam(1, $user, PDO::PARAM_STR);
+$sample->bindParam(2, $password, PDO::PARAM_STR);
+$row = get_row($sample);
+
 $blocked = blocked($username); //CWE-307 (Improper Restriction of Excessive Authentication Attempts (add attempts count with time restriction))
 
-if ($sample->num_rows() == 1 && !$blocked)
-    succeed();
+if ($sample->row_count() == 1 && !$blocked)
+    succeed($username);
 
 else if (!$blocked)
-    failed();
+    failed($username);
 
 ?>
